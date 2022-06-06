095229000c0d646d24c076a8cdcadea6925c12be
YanVologzhanin <yan.vologzhanin@target.com>
2022-06-06 12:27:52 -0500

    Initial Commit

---
+++ OISManager.py
@@ -0,0 +1,43 @@
+import yaml
+from neo4j import GraphDatabase
+from notion_client import Client
+
+from SharedUtilities import loadCachedData
+
+pages = {
+    'notes': 'de64f103da6b4cdfb1ea155a5d6b9ed7',
+    'references': '4bc3c2e0d9ef42038c52787f3e909c46'
+}
+
+with open('secrets/secrets.yml','r') as stream:
+    config = yaml.safe_load(stream)
+notion = Client(auth=config['token'])
+note_data = loadCachedData(notion, pages['notes'])
+references_data = loadCachedData(notion, pages['references'])
+
+neo4jDriver = GraphDatabase.driver('bolt://localhost:7687',auth=('neo4j', config['neo4jPassword']))
+# First Pass, Create Nodes
+node_count = 0
+
+with neo4jDriver.session() as session:
+    # for note in note_data:
+    #     note_properties = note['properties']
+    #     session.run(f"CREATE (n:Note:{note_properties['Note Type']['select']['name']} {{title:\"{note_properties['Name']['title'][0]['text']['content']}\",created: datetime(\"{note_properties['Created']['created_time']}\"), id: \"{note['id']}\"}})")
+    #     node_count = node_count + 1
+    #     print(f"Creating node #{node_count}")
+    # for reference in references_data:
+    #     reference_properties = reference['properties']
+    #     session.run(f"CREATE (n:Reference {{url:\"{reference_properties['Link']['url']}\", id:\"{reference['id']}\"}})")
+    #     node_count = node_count + 1
+    #     print(f"Creating node #{node_count}")
+    # # Second Pass, Create Relationships
+    relation_count = 0
+    for note in note_data:
+        note_properties = note['properties']
+        for child in note_properties['Child']['relation']:
+            session.run(f"MATCH (a:Note), (b:Note)  WHERE a.id='{note['id']}' AND b.id='{child['id']}' CREATE (a)-[r:PARENT_TOPIC]->(b)")
+            relation_count = relation_count + 1
+            print(f"Creating Relation #{relation_count}")
+    # for reference in references_data:
+    #     pass
+neo4jDriver.close()
---
+++ SharedUtilities.py
@@ -0,0 +1,61 @@
+import pickle
+from os.path import exists
+
+from neo4j import GraphDatabase
+
+
+def getFullTable(client, tableID, filter=None):
+    page = 0
+    all_results = []
+    if filter is not None:
+        current_result = client.databases.query(tableID, filter=filter)
+    else:
+        current_result = client.databases.query(tableID)
+    while current_result['has_more']: # Get and add all pages
+        page = page + 1
+        print(f"Loading page {page}")
+        all_results.extend(current_result['results'])
+        if filter:
+            current_result = client.databases.query(tableID, filter=filter, start_cursor=current_result['next_cursor'])
+        else:
+            current_result = client.databases.query(tableID, start_cursor=current_result['next_cursor'])
+    all_results.extend(current_result['results']) # Add final page
+    print('Done Loading!')
+    return all_results
+
+
+def loadCachedData(client, tableID, filter=None):
+    if not exists(tableID):
+        data = getFullTable(client, tableID, filter)
+        with open(tableID, 'wb') as file:
+            pickle.dump(data, file)
+    else:
+        with open(tableID, 'rb') as file:
+            data = pickle.load(file)
+    return data
+
+
+# class Neo4JDatabase:
+#     def __init__(self, uri, user, password):
+#         self.driver = GraphDatabase.driver(uri, auth=(user, password))
+#
+#     def close(self):
+#         self.driver.close()
+#
+#     def print_greeting(self, message):
+#         with self.driver.session() as session:
+#             greeting = session.write_transaction(self._create_and_return_greeting, message)
+#             return greeting
+#
+#
+#     @staticmethod
+#     def _create_and_return_greeting(tx, message):
+#         result = tx.run("CREATE (a:Greeting) "
+#                         "SET a.message = $message "
+#                         "RETURN a.message + ', from node ' + id(a)", message=message)
+#         return result.single()[0]
+
+
+# class PersonalNotionClient:
+#     def __init__(self, token):
+#         self.client = 0
